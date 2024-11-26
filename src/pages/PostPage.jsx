389c0daac77a5a2617ef790a2ca6aba940ec1c75
Gargenziano03 <gargenziano03@gmail.com>
2024-11-26 17:39:19 +0100

    add useParams

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,3 +1,11 @@
+import { useParams } from "react-router-dom"
 export default function PostPage() {
-    return
+    const { id } = useParams();
+    return (
+        <>
+            <h1>
+                post id:
+            </h1>
+        </>
+    )
 }
